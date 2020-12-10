09fbebcce86515393325799672b504de7e46dd1e
Jim Wang @ Intel <yutsung.jim.wang@intel.com>
2020-12-10 16:08:39 -0700

    feat(security): Remove Vault's healthy check in Consul (#40)
    The Vault's backend changes from consul to filesystem and the Vault's healthy check now becomes superfluous.
      The automatically generated healthy check in Vault now is gone due to Vault's backend storage is not dependent on Consul any more.
      This together with edgex-go's change shall remove the dependency of Vault from Consul.
    
    Signed-off-by: Jim Wang <yutsung.jim.wang@intel.com>

--- scripts/security-secretstore-setup-checker.sh
+++ scripts/security-secretstore-setup-checker.sh
@@ -1,7 +1,11 @@
 #!/bin/sh -ex
 
 # vault will be reported as healthy when it is unsealed
-/consul/scripts/consul-svc-healthy.sh vault
+# NOTE: comment out the vault healthy check below after we switch Vault to use 
+# file backend as the check becomes superfluous
+# the real check "SECRETSTORE_SETUP_DONE_FLAG" below already 
+# waited on Vault successfully initialized at the first place
+#/consul/scripts/consul-svc-healthy.sh vault
 
 # If SECRETSTORE_SETUP_DONE_FLAG not defined, pass; else
 # If SECRETSTORE_SETUP_DONE_FLAG file doesn't exist, error
