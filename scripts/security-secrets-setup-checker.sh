dab4574072ead8159af6d88b8c6e9e4dd7878cf2
Jim Wang @ Intel <yutsung.jim.wang@intel.com>
2020-12-17 08:47:55 -0700

    feat(security): Remove secrets setup checkers (#42)
    * feat(security): Remove Vault's healthy check in Consul
    
      The Vault's backend changes from consul to filesystem and the Vault's healthy check now becomes superfluous.
      The automatically generated healthy check in Vault now is gone due to Vault's backend storage is not dependent on Consul any more.
      This together with edgex-go's change shall remove the dependency of Vault from Consul.
    
    * feat(security): Remove the health checkers related to security-secrets-setup
    
    Once the edgex-go PR: Eliminate the security-secrets-setup module/component is removed, the related health checkers also need to be removed.
    
    Closes: #41
    
    Signed-off-by: Jim Wang <yutsung.jim.wang@intel.com>

--- scripts/security-secrets-setup-checker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -ex
-
-# check that the security-secrets-setup complete
-# the new way of checking just the sentinel files which will have all TLS assets
-test -f /tmp/edgex/secrets/ca/.security-secrets-setup.complete
